804839d7968d6dfdd2711778ba38e2554f74a252
theknickerbocker <krathbun@justworks.com>
2024-05-08 18:07:39 -0400

    update project structure

---
+++ cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	port := ":7777"
+
+	log.Print("starting server on ", port)
+
+	err := http.ListenAndServe(port, mux)
+	log.Fatal(err)
+}
