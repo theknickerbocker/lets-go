4e53422f589f91f18514eb5cbe6138c582a10633
theknickerbocker <krathbun@justworks.com>
2024-05-10 18:09:40 -0400

    add in static files and CLI flag arguments

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":7777", "HTTP network address")
+	static_dir := flag.String("static", "ui/static/", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir(*static_dir))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	port := ":7777"
+	log.Print("starting server on ", *addr)
 
-	log.Print("starting server on ", port)
-
-	err := http.ListenAndServe(port, mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
