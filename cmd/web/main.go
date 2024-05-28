6fc4d1f6b547d676bc89dc13d8e966895f2ae4bb
theknickerbocker <krathbun@justworks.com>
2024-05-28 15:19:46 -0400

    Add partial database functionality

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"github.com/theknickerbocker/snippetbox/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":7777", "HTTP network address")
+	dsn := flag.String("dsn", "web:Milo1234@/snippetbox?parseTime=true", "MySQL data source name")
 	static_dir := flag.String("static", "ui/static/", "Path to static assets")
 	flag.Parse()
 
@@ -20,13 +27,36 @@ func main() {
 		AddSource: true,
 	}))
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	app := &application{
-		logger: logger,
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes(static_dir))
+	err = http.ListenAndServe(*addr, app.routes(static_dir))
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
